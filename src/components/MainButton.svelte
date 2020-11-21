3900dc5eb5b265d0a13ee79186bebb3ea4bb7348
Anastasia Gisina <black_brain.vip@bk.ru>
2020-11-21 20:14:55 +0300

    add main button

---
+++ src/components/MainButton.svelte
@@ -0,0 +1,27 @@
+<script>
+    import {changePage} from '../stores';
+    export let button_text = "Select";
+  </script>
+  
+  <button on:click={() => changePage(-1)}>{button_text}</button>
+  
+  <style>
+    button {
+      background-color: #3C9294;
+      width: 260px;
+      height: 58px;
+      color: #FFFFFF;
+      border: none;
+      transition: background-color 0.5s ease;
+      border-radius: 50px; 
+      align-items: center;
+      justify-content: center;
+      font-weight: 600;
+    }
+    button:hover {
+      background: #f4f4f4;
+    }
+    button:focus {
+      background: gainsboro;
+    }
+  </style>
---
+++ src/pages/Third.svelte
@@ -0,0 +1,9 @@
+<script>
+    import BackButton from '../components/BackButton.svelte';
+    import MainButton from '../components/MainButton.svelte';
+</script>
+
+<main>
+    <BackButton/>
+    <MainButton></MainButton>
+</main>
