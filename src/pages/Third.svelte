9ba28f09727a23a28f1ff87d45f7c20db4bf9817
Anastasia Gisina <black_brain.vip@bk.ru>
2020-11-21 21:33:27 +0300

    change third page

--- src/pages/Third.svelte
+++ src/pages/Third.svelte
@@ -2,13 +2,12 @@
   import BackButton from '../components/BackButton.svelte'
   import MainButton from '../components/MainButton.svelte'
   import {chosenLocation} from '../stores'
-
-  chosenLocation.set("0")
 </script>
 
 <h1>Page 3</h1>
 <BackButton/>
-<button on:click={() => chosenLocation.set('Party')} selected class="select-button">
+
+<button on:click={() => chosenLocation.set('Party')} style="border-color: selected={$chosenLocation == "Party" ? 'green' : 'transparent'}" class="select-button">
     <img src="./img/community.svg" alt="party" />
     <h1>Party</h1>
 </button>
@@ -17,8 +16,7 @@
     <h1>Political debate</h1>
 </button>
 
-<h1>{$chosenLocation}</h1>
-<MainButton></MainButton>
+<MainButton disabled={$chosenLocation == ""}></MainButton>
 
 <style>
     .select-button {
